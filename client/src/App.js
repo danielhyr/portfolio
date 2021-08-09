da170d280913b4614e5bb48e13ec6c9dd59526c0
danielhyr <danhyunrha@gmail.com>
2021-08-08 23:10:05 -0400

    final fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import logo from './logo.svg';
 import './App.scss';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import MainPage from './components/MainPage/MainPage'
 import PortfolioPage from './components/PortfolioPage/PortfolioPage';
 import AboutPage from './components/AboutPage/AboutPage';
 import ContactPage from './components/ContactPage/ContactPage';
+import { AnimatePresence, motion } from 'framer-motion'
+import { useLocation } from "react-router-dom";
 
 function App() {
+
+  const location = useLocation();
+
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path = "/" component = {MainPage}/>
-        <Route exact path = "/portfolio" component = {PortfolioPage}/>
-        <Route exact path = "/about" component = {AboutPage}/>
-        <Route exact path = "/contact" component = {ContactPage}/>
+    <AnimatePresence initial={true} exitBeforeEnter >
+      <Switch location={location} key={location.pathname}>
+
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/portfolio" component={PortfolioPage} />
+
+        <Route exact path="/about" component={AboutPage} />
+
+        <Route exact path="/contact" component={ContactPage} />
       </Switch>
-    </BrowserRouter>
+    </AnimatePresence>
   );
 }
 
--- client/src/components/subcomponents/Header/Header.jsx
+++ client/src/components/subcomponents/Header/Header.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
 import './Header.scss'
-import { useHistory } from 'react-router-dom' 
+import { useHistory } from 'react-router-dom'
+
+import { motion } from 'framer-motion'
 
 function Header() {
-  const  history = useHistory()
+    const history = useHistory()
+    const duration = {duration: .6, ease: [.6, .01, -0.05, 0.9] }
 
     return (
-        <ul className="nav">
-            <li className="nav__list" onClick = {() => history.push('/')}>
-                Home
-            </li>
-            <li className="nav__list" onClick = {() => history.push('/about')}>
-                About
-            </li>
-            <li className="nav__list" onClick = {() => history.push('/portfolio')}>
-                Portfolio
-            </li>
-            <li className="nav__list" onClick = {() => history.push('/contact')}>
-                Contact
-            </li>
-        </ul>
+        <>
+
+            <motion.ul 
+            whileHover={{scale:1.05}}
+            transition= {duration}
+            className="nav">
+
+                <div className="nav__liWrap" >
+                    <li className="nav__list" onClick={() => history.push('/')}>
+                        Home
+                    </li>
+                </div>
+                <div className="nav__liWrap" >
+                    <li className="nav__list" onClick={() => history.push('/about')}>
+                        About
+                    </li>
+                </div>
+                <div className="nav__liWrap" >
+                    <li className="nav__list" onClick={() => history.push('/portfolio')}>
+                        Portfolio
+                    </li>
+                </div>
+                <div className="nav__liWrap" >
+                    <li className="nav__list" onClick={() => history.push('/contact')}>
+                        Contact
+                    </li>
+                </div>
+
+
+            </motion.ul>
+        </>
     )
 }
 
